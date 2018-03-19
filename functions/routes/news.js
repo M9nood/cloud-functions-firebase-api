18f7b7a3981759df3f6a655a4092d3e7adb12a56
M9nood <spw.ntw@gmail.com>
2018-03-19 17:23:57 +0700

    Update news.js

--- functions/routes/news.js
+++ functions/routes/news.js
@@ -3,12 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 var config = {
-  apiKey: "AIzaSyDa_6thYTOzbbhFrKxlzZ3QPUDHjmIdDyk",
-  authDomain: "midterm-news.firebaseapp.com",
-  databaseURL: "https://midterm-news.firebaseio.com",
-  projectId: "midterm-news",
-  storageBucket: "midterm-news.appspot.com",
-  messagingSenderId: "633739253627"
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
 };
 var firebaseApp = firebase.initializeApp(config)
 var db = firebaseApp.database()
